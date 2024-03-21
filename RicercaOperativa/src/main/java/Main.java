72dae1ea172ef5aae219c5f4d07e73d5c508b936
Luca <129498542+LucaFerraro03@users.noreply.github.com>
2024-03-21 22:58:15 +0100

    Update Main.java

--- RicercaOperativa/src/main/java/Main.java
+++ RicercaOperativa/src/main/java/Main.java
@@ -2,14 +2,23 @@ public class Main {
 
     public static void main(String[] args) {
 
-        double[][] matrix = {{1, -2, -6, 0, 0, 0, 0, 0},
+        double[][] matrix1 = {{1, -2, -6, 0, 0, 0, 0, 0},
                 {1, 0, 0, 1, 0, 0, 0, 2},
                 {0, 1, 0, 0, 1, 0, 0, 3},
                 {0, 0, 1, 0, 0 , 1, 0, 3},
                 {1, 1, 1, 0, 0, 0, 1, 4}};
 
-        Tableau tableau = new Tableau(matrix, "min");
+        double[][] matrix2 = {{1, 3, 0, 0, 0},
+                             {1, -2, 1, 0, 4},
+                             {1, -1, 0, 1, 8}};
 
+        Tableau tableau;
+
+        tableau = new Tableau(matrix1, "max");
+        tableau.solve();
+
+
+        tableau = new Tableau(matrix2, "max");
         tableau.solve();
     }
 }
